07a21cf19c522af14cd3a80d47d301da057c3d24
Amy Fogelman <abfnj1@gmail.com>
2019-05-15 18:13:04 +0000

    Done.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,11 @@
 require_relative './spec_helper.rb'
-# We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
+require_relative '../fizzbuzz.rb'
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
+    
+# We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
 
     expect(fizz_3).to eq("Fizz")
   end
